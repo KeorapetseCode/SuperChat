d6bfabd49d865c4331c7dbc9c71de67b6b3037c1
KeorapetseCode <kmpoloke@student.wethinkcode.co.za>
2022-12-15 10:56:35 +0200

    hid firebase API keys

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { initializeFirestore } from "firebase/firestore";
+<<<<<<< HEAD
 import Constants from "expo-constants";
 
 //need to use .env to safely use API keys and push the file to remote repo 
@@ -12,6 +13,16 @@ const firebaseConfig = {
   storageBucket: Constants.manifest.extra.storageBucket,
   messagingSenderId: Constants.manifest.extra.messagingSenderId,
   appId: Constants.manifest.extra.appId
+=======
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD44ZH1oWZ7loJl08l6Uc4ULI71NLJl3Xs",
+  authDomain: "superchat-7f8cf.firebaseapp.com",
+  projectId: "superchat-7f8cf",
+  storageBucket: "superchat-7f8cf.appspot.com",
+  messagingSenderId: "682183974118",
+  appId: "1:682183974118:web:4bae316708d654eae62f27"
+>>>>>>> parent of 980a708... deleted firebase because of api keys were exposed
 };
 
 export const app = initializeApp(firebaseConfig);
