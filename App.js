3865ad730f7f0f53eb62b6b3dd8d5502ec52f7f8
KeorapetseCode <kmpoloke@student.wethinkcode.co.za>
2022-12-15 12:46:16 +0200

    added log box to ignore old React-Native errors and warnings

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { useAssets } from "expo-asset";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, LogBox } from 'react-native';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from "./firebase";
+
+//Logbox. Error loging system that React Native uses. Certain Firebases errors that we are recommended to ignore
+/*
+LogBox.ignoreLogs([
+  "Setting a timer",
+  "AsyncStorage has been extracted from react-native core and will be removed in a future release.",
+]);*/
+
 
 function App() {
   const [currUser, setCurrUser] = useState(null);
@@ -25,7 +33,7 @@ function App() {
   else{
     return (
       <View style={styles.container}>
-        <Text>{JSON.stringify(currUser)}</Text>
+        <Text>It is me {JSON.stringify(currUser)}</Text>
         <StatusBar style="auto" />
       </View>
     );
